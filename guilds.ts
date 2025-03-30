260660c6a44ec1c9c8f63822396277b81738e4d6
Robin <robinhallerbach13@googlemail.com>
2025-03-30 22:57:40 +0200

    guild updated

--- guilds.ts
+++ guilds.ts
@@ -10,11 +10,17 @@ const GUILDS: GuildConfig[] = [
 			},
 		},
 	},
-	/* {
+	{
 		id: '1059613070785257494', //illu
 		goldRole: '1354157905473179860',
 		dropRole: '1059634799679115285',
-	}, */
+		minesweeper: {
+			nightTime: {
+				start: 23,
+				end: 6,
+			},
+		},
+	}, 
 ]
 export default GUILDS
 
